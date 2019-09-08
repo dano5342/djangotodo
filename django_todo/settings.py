e965bc2e24d8993103446a5d7169eaf69d7394d9
Ubuntu <ubuntu@ip-172-31-92-69.ec2.internal>
2019-09-08 11:25:37 +0000

    Implement environment specific values for DEBUG and DATABASE

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+
+
+if os.environ.get('DEVELOPMENT'):
+    development = True
+else:
+    development = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,11 +30,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8!ecvui+iybm=+t5w=_al2h_e=)i)&f6tu2fhc8tl()9g=jnoo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
 ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"),
                  os.environ.get("HOSTNAME"),
-                 'django-to-do-dan.herokuapp.com']
+                 'django-to-do-dan.herokuapp.com',
+                 'd99c5b6f10e74b28af1f2a6f1a41d92f.vfs.cloud9.us-east-1.amazonaws.com']
 
 
 host = os.environ.get('SITE_HOST')
@@ -80,17 +88,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
-"""
-
-DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+else: 
+    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
